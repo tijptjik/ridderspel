e410ad79f4e28086a77d817e9b8e788547e5826e
Mart van de Ven <m@type.hk>
2013-03-01 12:53:26 +0800

    Deal number of cards corrected for number of hands played

--- games/ohhell.py
+++ games/ohhell.py
@@ -1,6 +1,7 @@
 #_*_ coding: utf-8 _*_
 from game  import Game, Player
 from cards import Deck, Card
+from math import floor, ceil
 
 class Rules(Game):
 	"""Rules for the game of Oh Hell!"""
@@ -11,7 +12,7 @@ class Rules(Game):
 		self.handLimit = [15,17,19,21,23]
 		super(Rules, self).__init__()
 		self.handCounter = 0
-		self.hands = [Hand(n) for n in range(self.handLimit)]
+		self.hands = [Hand(n, self.handLimit) for n in range(self.handLimit)]
 	
 	def setupDeck(self):
 		for cards in [(0,2),(1,2),(2,2),(3,2),(0,3),(1,3),(2,3),(3,3),(0,4),(1,4)]:
@@ -45,14 +46,14 @@ class Hand(object):
 	   is reached. 
    
 	   Trumps proceed from Spades, Diamons, Clubs, Hearts to No Trumps"""
-	def __init__(self, n):
+	def __init__(self, n, handLimit):
 		super(Hand, self).__init__()
 		self.trumpSuits = [u'♠',u'♦',u'♣',u'♥',None]
 		self.counter = n
 		self.trump = n % 5
-		if n > 7:
+		if n > int(floor(float(handLimit)/2.0)):
 			n = n + 2
-		self.cardLimit = abs(8 - n)
+		self.cardLimit = abs(int(ceil(float(handLimit)/2.0)) - n)
 	def __str__(self):
 		return 'Hand ' + str(self.counter) + ':' + ' ' + str(self.cardLimit) + ' ' + str(self.trump)
 	def __unicode__(self):	
